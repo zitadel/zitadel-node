b9cb7bcc45ec16d22e33abd572ef8fc1ac94d3cc
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-06-08 11:47:57 +0300

    chore: update generated output under .openapi markers

---
+++ src/apis/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ActionServiceApi.js';
+export * from './FeatureServiceApi.js';
+export * from './IdentityProviderServiceApi.js';
+export * from './OIDCServiceApi.js';
+export * from './OrganizationServiceApi.js';
+export * from './SAMLServiceApi.js';
+export * from './SessionServiceApi.js';
+export * from './SettingsServiceApi.js';
+export * from './UserServiceApi.js';
+export * from './WebKeyServiceApi.js';
