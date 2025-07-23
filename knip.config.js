7cc6602ae015ef1773ed3acbac0be33bb703b5e8
Mridang Agarwalla <mridang@zitadel.com>
2025-07-23 13:49:29 +0530

    feat(build): added a workflow to find unused dependencies (#55)
    * feat(build): added a workflow to find unused dependencies
    
    * fix(build): ingore the false-postives about the unused deps

--- knip.config.js
+++ knip.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  entry: ['src/index.ts', 'release.config.mjs'],
+  entry: ['src/index.ts', 'release.config.mjs', 'commitlint.config.mjs'],
   ignoreDependencies: [
     '@semantic-release/.*?',
     '@mridang/semantic-release-peer-version',
     '@codedependant/semantic-release-docker',
+    '@commitlint/.*?',
   ],
-  ignore: ['src/apis/**', 'src/models/**'],
+  ignore: ['src/apis/**', 'src/models/**', 'comm'],
 };
